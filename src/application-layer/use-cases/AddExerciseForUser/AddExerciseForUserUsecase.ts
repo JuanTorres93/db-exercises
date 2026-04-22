7c3a738c00a562c81d483c5c5bfd19c0c5612b62
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-04-22 14:48:28 +0200

    Created AddExerciseForUserUsecase

---
+++ src/application-layer/use-cases/AddExerciseForUser/AddExerciseForUserUsecase.ts
@@ -0,0 +1,51 @@
+import {
+  ExerciseDTO,
+  toExerciseDTO,
+} from "@/application-layer/dtos/ExerciseDTO";
+import { IdGenerator } from "@/application-layer/services/IdGenerator.port";
+import { AlreadyExistsError, ValidationError } from "@/domain/common/errors";
+import { Exercise } from "@/domain/entities/exercise/Exercise";
+import { ExercisesRepo } from "@/domain/repos/ExercisesRepo.port";
+
+export type AddExerciseForUserUsecaseRequest = {
+  name: string;
+  userId: string;
+};
+
+export class AddExerciseForUserUsecase {
+  constructor(
+    private exercisesRepo: ExercisesRepo,
+    private idGenerator: IdGenerator,
+  ) {}
+
+  async execute(
+    request: AddExerciseForUserUsecaseRequest,
+  ): Promise<ExerciseDTO> {
+    if (!request.userId) {
+      throw new ValidationError(
+        "AddExerciseForUserUsecase: User ID is required",
+      );
+    }
+
+    const existingExercise = await this.exercisesRepo.getByNameAndUserId(
+      request.name,
+      request.userId,
+    );
+
+    if (existingExercise) {
+      throw new AlreadyExistsError(
+        `AddExerciseForUserUsecase: Exercise with name "${request.name}" already exists for userId "${request.userId}".`,
+      );
+    }
+
+    const exercise = Exercise.create({
+      id: this.idGenerator.generateId(),
+      name: request.name,
+      userId: request.userId,
+    });
+
+    await this.exercisesRepo.save(exercise);
+
+    return toExerciseDTO(exercise);
+  }
+}
---
+++ src/application-layer/use-cases/AddExerciseForUser/__tests__/AddExerciseForUserUsecase.test.ts
@@ -0,0 +1,114 @@
+import { AlreadyExistsError, ValidationError } from "@/domain/common/errors";
+import { Exercise } from "@/domain/entities/exercise/Exercise";
+import { MemoryExercisesRepo } from "@/infra/repos/memory/MemoryExercisesRepo";
+import { Uuidv4IdGenerator } from "@/infra/services/Uuidv4IdGenerator/Uuidv4IdGenerator";
+
+import { createTestExercise } from "../../../../../tests/createProps/exerciseTestProps";
+import { exerciseDTOProperties } from "../../../../../tests/dtoProperties/exerciseDtoProperties";
+import { AddExerciseForUserUsecase } from "../AddExerciseForUserUsecase";
+
+describe("AddExerciseForUserUsecase", () => {
+  let exercisesRepo: MemoryExercisesRepo;
+  let idGenerator: Uuidv4IdGenerator;
+
+  let usecase: AddExerciseForUserUsecase;
+  let exercise: Exercise;
+
+  beforeEach(async () => {
+    exercisesRepo = new MemoryExercisesRepo();
+    idGenerator = new Uuidv4IdGenerator();
+
+    usecase = new AddExerciseForUserUsecase(exercisesRepo, idGenerator);
+
+    exercise = createTestExercise({
+      userId: "user-1",
+    });
+  });
+
+  describe("Execution", () => {
+    it("should return ExerciseDTO", async () => {
+      const result = await usecase.execute({
+        name: exercise.name,
+        userId: exercise.userId!,
+      });
+
+      expect(result).not.toBeInstanceOf(Exercise);
+      for (const prop of exerciseDTOProperties) {
+        expect(result).toHaveProperty(prop);
+      }
+    });
+
+    it("should generate different ids for different exercises", async () => {
+      const otherExerciseDTO = await usecase.execute({
+        name: "Another Exercise",
+        userId: exercise.userId!,
+      });
+
+      const anotherExerciseDTO = await usecase.execute({
+        name: "Yet Another Exercise",
+        userId: exercise.userId!,
+      });
+
+      expect(otherExerciseDTO.id).not.toBe(exercise.id);
+      expect(anotherExerciseDTO.id).not.toBe(exercise.id);
+      expect(anotherExerciseDTO.id).not.toBe(otherExerciseDTO.id);
+    });
+  });
+
+  describe("Side effects", () => {
+    it("should persist exercise in repo", async () => {
+      const exerciseDTO = await usecase.execute({
+        name: exercise.name,
+        userId: exercise.userId!,
+      });
+
+      const foundExercise = await exercisesRepo.getById(exerciseDTO.id);
+
+      expect(foundExercise).toBeInstanceOf(Exercise);
+      expect(foundExercise!.id).toBe(exerciseDTO.id);
+      expect(foundExercise!.name).toBe(exerciseDTO.name);
+      expect(foundExercise!.userId).toBe(exerciseDTO.userId);
+    });
+  });
+
+  describe("Errors", () => {
+    it("should throw error if userId is not provided", async () => {
+      await expect(() =>
+        usecase.execute({
+          name: exercise.name,
+          userId: "",
+        }),
+      ).rejects.toThrow(ValidationError);
+
+      await expect(() =>
+        usecase.execute({
+          name: exercise.name,
+          userId: "",
+        }),
+      ).rejects.toThrow(/AddExerciseForUserUsecase.*ID is required/);
+    });
+
+    it("should throw error if name already exists for user id", async () => {
+      const name = "Exercise Name";
+      const userId = "user-1";
+
+      await usecase.execute({
+        name,
+        userId,
+      });
+
+      await expect(() =>
+        usecase.execute({
+          name,
+          userId,
+        }),
+      ).rejects.toThrow(AlreadyExistsError);
+      await expect(() =>
+        usecase.execute({
+          name,
+          userId,
+        }),
+      ).rejects.toThrow(/AddExerciseForUserUsecase.*already exists/);
+    });
+  });
+});
